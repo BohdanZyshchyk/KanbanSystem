bde6dfe00b7b3201f1fd76b4b6ab0ed659d36755
Kerfoer <bartolomio177@gmail.com>
2020-05-12 19:27:27 +0300

    user callback

---
+++ KanBanSystem/ClientUI/Callbacks/UserCallback.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientUI.KrabServicesUserManager;
+
+namespace ClientUI.Callbacks
+{
+    public class UserCallback : IUserManagerServiceCallback
+    {
+        public void InformAboutLogin(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InformAboutRegistration(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RefreshBoards(BoardDTO[] boards)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
